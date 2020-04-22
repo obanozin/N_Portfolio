117412c4a23cdc2a69ee5b0aa62a72abdcb937a3
obanozin <obanozin18@gmail.com>
2020-04-22 21:42:48 +0900

    [add]CSSで画面遷移機能を追加

--- script.js
+++ script.js
@@ -1,21 +1,6 @@
 
 $(function(){
     $('.main-title').fadeIn(2000);
-    imagesChange();
-    function imagesChange (){
-        var images = [ `url(images/top0.jpg)`, `url(images/top1.jpg)`];
-        var backgroundRandom = function(){
-            $('.main-top').delay(5000).queue(function(next) {
-                $(this).css('background-image',images[1]);
-                next();
-            });
-            $('.main-top').delay(5000).queue(function(next) {
-                $(this).css('background-image',images[0]);
-                next();
-            });
-        }
-        setInterval(backgroundRandom, 0);        
-    }
 });
 
 
