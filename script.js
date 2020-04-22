66e0d3733fe9b1eb2e6d63594a4a97f21d96fc09
obanozin <obanozin18@gmail.com>
2020-04-22 20:21:04 +0900

    [add]画面遷移追加

--- script.js
+++ script.js
@@ -1,38 +1,21 @@
-$(function() {
+
+$(function(){
     $('.main-title').fadeIn(2000);
-    
-    
-
-
-    //$(window).on('load scroll', function (){
-      //$(window).scroll(function (){
-        //$('.fadein').each(function(){
-            //var position = $(this).offset().top;
-            //var scroll = $(window).scrollTop();
-            //var windowHeight = $(window).height();
-            //if (scroll > position - windowHeight + 300){
-                //$(this).addClass('active');
-            //};
-        //});
-    //});
-   //});
-  });
-
-
-var picks_src = new Array("images/IMG_20190313_130836.jpg", "images/IMG_20190308_125356.jpg", "images/IMG_20150628_150706.jpg");
-var num = -1
-
-slideshow_timer();
-
-function slideshow_timer(){
-    if (num == 2){
-        num = 0
+    imagesChange();
+    function imagesChange (){
+        var images = [ `url(images/top0.jpg)`, `url(images/top1.jpg)`];
+        var backgroundRandom = function(){
+            $('.main-top').delay(5000).queue(function(next) {
+                $(this).css('background-image',images[1]);
+                next();
+            });
+            $('.main-top').delay(5000).queue(function(next) {
+                $(this).css('background-image',images[0]);
+                next();
+            });
+        }
+        setInterval(backgroundRandom, 0);        
     }
-    else {
-        num ++;
-    }
-    document.getElementById("top-slide").src=picks_src[num];
-    setTimeout("slideshow_timer()", 1000); 
-}
+});
 
-  //https://linkage-design.net/jquery-scroll-fadein
+
